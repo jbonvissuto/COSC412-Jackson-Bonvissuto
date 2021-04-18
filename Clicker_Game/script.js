652df8201164d10c6f9ba8aef0744f8df15a0660
Batman <jackakabatman@gmail.com>
2021-04-18 18:10:17 -0400

    added line endings

--- Clicker_Game/script.js
+++ Clicker_Game/script.js
@@ -3,36 +3,36 @@
 let x
 //This needs to grab from the database not local storage 
 var highscore = localStorage.getItem("highscore");
-document.getElementById("highscore").innerHTML = highscore
+document.getElementById("highscore").innerHTML = highscore;
 // if the key value pair exists, we set x equal to the stored value
 // else, set x to 0
 if (localStorage.getItem("count")){
-    x = parseInt(localStorage.getItem("count"))
+    x = parseInt(localStorage.getItem("count"));
 } else{
-    x = 0
+    x = 0;
 }
 
-document.getElementById('btn').addEventListener("click", iterate)
-document.getElementById("counter").innerHTML = x
+document.getElementById('btn').addEventListener("click", iterate);
+document.getElementById("counter").innerHTML = x;
 function iterate(){
-    x += 1
-    localStorage.setItem("count", x)
-    console.log(x)
-    document.getElementById("counter").innerHTML = x
+    x += 1;
+    localStorage.setItem("count", x);
+    console.log(x);
+    document.getElementById("counter").innerHTML = x;
 }
 
 
-document.getElementById('btn2').addEventListener("click", reset)
+document.getElementById('btn2').addEventListener("click", reset);
 function reset(){
-    x = 0
-    localStorage.setItem("count", x)
-    console.log(x)
-    document.getElementById("counter").innerHTML = x
+    x = 0;
+    localStorage.setItem("count", x);
+    console.log(x);
+    document.getElementById("counter").innerHTML = x;
 }
 
 
 
-document.getElementById('btn3').addEventListener("click", highScore)
+document.getElementById('btn3').addEventListener("click", highScore);
 function highScore(){
     //const name = prompt('You got a high score! Enter name:');
     //This needs to grab from the database not local storage 
@@ -42,22 +42,23 @@ function highScore(){
             //This needs to set the value in the database
             localStorage.setItem("highscore", x); 
             
-            document.getElementById("highscore").innerHTML = x    
+            document.getElementById("highscore").innerHTML = x;    
         }
     }
     else{
         //This needs to set the value in the database
         localStorage.setItem("highscore", x);
         
-        document.getElementById("highscore").innerHTML = x  
+        document.getElementById("highscore").innerHTML = x;  
     }
 }
 
-document.getElementById('btn4').addEventListener("click", resetScore)
+document.getElementById('btn4').addEventListener("click", resetScore);
 function resetScore(){
-    highscore = 0
+    highscore = 0;
     //This needs to set the value in the database
-    localStorage.setItem("highscore", highscore)
-    console.log(highscore)
-    document.getElementById("highscore").innerHTML = highscore
+    localStorage.setItem("highscore", highscore);
+    console.log(highscore);
+    document.getElementById("highscore").innerHTML = highscore;
 }
+
