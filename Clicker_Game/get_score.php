5bf85f22c93c4a59c54934baec66ec611f93025d
Batman <jackakabatman@gmail.com>
2021-04-23 19:22:04 -0400

    added stuff

--- Clicker_Game/get_score.php
+++ Clicker_Game/get_score.php
@@ -9,12 +9,9 @@ if ($conn->connect_error) {     // Check connection
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$highscore = mysqli_real_escape_string($conn, $_POST['score']);//Gets the value of the passed variable after score:
-
-if (strlen($times) > 200000) {  $times = "";    }//Checks if to many requests or something 
-
 $sql = "SELECT high_score FROM highscore WHERE ID=1 ;"; //Get the high schore from the database 
-echo $row['high_score'];
+$result = $conn->query($sql);
+echo $result['high_score'];
 echo "test\n";
 $conn->close();
 ?>
