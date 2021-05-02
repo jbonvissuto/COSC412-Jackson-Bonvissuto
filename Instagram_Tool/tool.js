31bc6ef173aba5e16d86c3a3cbc61cde5cd21bc0
Batman <jackakabatman@gmail.com>
2021-05-02 15:46:32 -0400

    added display of average likes and comments

--- Instagram_Tool/tool.js
+++ Instagram_Tool/tool.js
@@ -1,4 +1,6 @@
 //on loading tool.js display the stats 
+var avgLikes = localStorage.getItem('likes')/localStorage.getItem('postNum');
+var avgComments = localStorage.getItem('comments')/localStorage.getItem('postNum');
 window.onload = function what(){
     document.getElementById("username").innerHTML = localStorage.getItem('username');
     document.getElementById("fullName").innerHTML = localStorage.getItem('fullName');
@@ -7,4 +9,6 @@ window.onload = function what(){
     document.getElementById("followsNum").innerHTML = localStorage.getItem('followsNum');
     document.getElementById("likes").innerHTML = localStorage.getItem('likes');
     document.getElementById("comments").innerHTML = localStorage.getItem('comments');
+    document.getElementById("avgLikes").innerHTML = avgLike;
+    document.getElementById("avgComments").innerHTML = avgComments;
   }
